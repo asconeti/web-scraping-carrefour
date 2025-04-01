540459b29d884a3aeb8b9ea25f9aa0062466adce
Asconeti <jordibaigesferre@gmail.com>
2025-04-01 05:12:15 +0000

    Actualització def container

--- web-scraping-practica1/source/main.py
+++ web-scraping-practica1/source/main.py
@@ -1,1 +1,15 @@
-print ("Hello world")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.carrefour.es/supermercado/bebidas/cat20005/c'
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Exemple: extreure noms de productes
+products = soup.find_all('a', class_='product-card__title')
+for product in products:
+    print(product.text.strip())
